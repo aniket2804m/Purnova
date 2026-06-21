acdb257bdbe8e2a5eb627d04cdb2a7df69db9f5c
Aniket <suryawanshianiket7576@gmail.com>
2026-06-21 17:12:53 +0530

    updat

--- src/App.tsx
+++ src/App.tsx
@@ -45,7 +45,7 @@ const App = () => {
 
         <BrowserRouter>
           <LoadingScreen isLoading={isLoading} />
-          {/* <ScrollToTop /> */}
+          <ScrollToTop />
           <CursorGlow />
           <ScrollProgress />
 
